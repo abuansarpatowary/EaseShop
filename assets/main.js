d7d21bfbcb6dc81da7b28e777bd8fec0d4a8193d
abu ansar <abuansr040@gmail.com>
2024-10-19 19:31:37 +0600

    Slider prev next working

--- assets/main.js
+++ assets/main.js
@@ -85,7 +85,12 @@
     loop: true,
     margin: 0,
     items: 1,
-    navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+    navText: [
+      `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18" />
+</svg>`,
+      '<i class="fa fa-long-arrow-right"></i>',
+    ],
     nav: true,
     dots: false,
     responsive: {
